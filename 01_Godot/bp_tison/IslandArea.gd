d7f6255c2591b7b3d584ce1f59050325a9124f8e
MartinTison <martin.tison101@gmail.com>
2025-04-15 13:46:48 +0200

    Delete 01_Godot/bp_tison directory

--- 01_Godot/bp_tison/IslandArea.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-# Funkcia sa spustí, keď do tejto oblasti vstúpi iný Area2D objekt
-func _on_area_entered(area: Area2D):
-	# Zničenie objektu, ktorý narazil do ostrova 
-	area.queue_free()
--- 01_Godot/bp_tison/help_menu.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-# Táto funkcia sa spustí, keď používateľ klikne na tlačidlo "Back"
-func _on_back_button_pressed():
-	# Prepne scénu späť na hlavné menu
-	get_tree().change_scene_to_file("res://main_menu.tscn")
